b60733e52f66eebf96b4724360cd0f72e1ae454e
ollielynas <lynasollie@gmail.com>
2022-04-04 14:13:39 +1200

    welp I made things worse

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -45,8 +45,8 @@ class Engine {
 
 let engine = new Engine({
     containerId: 'game',
-    canvasW: 3000,
-    canvasH: 3000,
+    canvasW: 2000,
+    canvasH: 2000,
     fpsMax: 60,
 });
 
@@ -85,6 +85,11 @@ let playerInfo:{[name:string]:any} = {
     upKey: "w",
     downKey: "s",
     jumpKey: " ",
+    // leftKey: "ArrowLeft",
+    // rightKey: "ArrowRight",
+    // upKey: "ArrowUp",
+    // downKey: "ArrowDown",
+    // jumpKey: "ArrowUp",
     canMagicDash: true,
     magicJuice: 50,
     magicJuiceMax: 50,
@@ -96,10 +101,10 @@ let playerInfo:{[name:string]:any} = {
     displayType: "hitbox",
     useBitmap: true,
     dev: true,
-    fly: false
 }
 
-
+let canFly = false;
+let disableFog = false;
 
 for (let i = 0; i < Object.keys(playerInfo).length; i++) {
 
@@ -117,8 +122,6 @@ const saveCookie =async()=> {
 
 window.onload = load;
 
-
-
 function load() {
     create();
 } // load
@@ -130,8 +133,8 @@ let staticLevelContainer = new PIXI.Container()
 
 backgroundImage.x  = 0;
 backgroundImage.y = 0;  
-staticLevelContainer.height = 3000;
-staticLevelContainer.width = 3000;
+staticLevelContainer.height = 2000;
+staticLevelContainer.width = 2000;
 engine.stage.addChild(staticLevelContainer);
 // |---------------------------------------------------- LOAD LEVEL ---------------------------------------|
 function loadLevel(filename:string) {
@@ -139,26 +142,41 @@ function loadLevel(filename:string) {
     playerInfo.lastLevel = filename;
     staticLevelContainer.cacheAsBitmap = false;
     staticLevelContainer.removeChildren();
+    engine.stage.removeChildren();
+    engine.stage.addChild(staticLevelContainer);
+
 
     console.log('images/'+filename+'-background.png');
     backgroundImage.texture = PIXI.Texture.from('images/'+filename+'-background.png');
-    backgroundImage.height = 3000
-    backgroundImage.width = 3000
+    backgroundImage.height = 2000
+    backgroundImage.width = 2000
 
-    let scaleX = 3000/window.innerWidth;
-    let scaleY = 3000/window.innerHeight;
+    let scaleX = 2000/window.innerWidth;
+    let scaleY = 2000/window.innerHeight;
     console.log(filename);
 
     engine.stage.scale = new PIXI.Point(scaleX*1.5, scaleY*1.5);
 
-    if (playerInfo.displayType == "hitbox") {
+    if (playerInfo.displayType == "hitbox") {  // |-------------- add physiks hitboxes
     for (let i = 0; i < levelData.phyBox.length; i++) {
         let obj = new PIXI.Graphics();
         obj.beginFill(0xffffff);
         obj.lineStyle(1, 0x000000);
         obj.drawRect(levelData.phyBox[i][0][0], levelData.phyBox[i][0][1], levelData.phyBox[i][1][0]-levelData.phyBox[i][0][0], levelData.phyBox[i][1][1]-levelData.phyBox[i][0][1]);
         staticLevelContainer.addChild(obj)
+        if (levelData.phyBox[i].length == 3) {
+            let label = new PIXI.Text(levelData.phyBox[i][2], {fontFamily: 'Arial', fontSize: 8, fill: 0x000000, align: 'center'});
+            label.x = levelData.phyBox[i][0][0];
+            label.y = levelData.phyBox[i][0][1];
+            staticLevelContainer.addChild(label);
+    }else{
+        let label = new PIXI.Text(JSON.stringify(levelData.phyBox[i]), {fontFamily: 'Arial', fontSize: 8, fill: 0x000000, align: 'center'});
+            label.x = levelData.phyBox[i][0][0];
+            label.y = levelData.phyBox[i][0][1];
+            staticLevelContainer.addChild(label);
     }
+
+}
 
     for (let i = 0; i < levelData.deathBox.length; i++) {
         let obj = new PIXI.Graphics();
@@ -187,8 +205,25 @@ function loadLevel(filename:string) {
         obj4.lineStyle(1, 0xc27b00);
         obj4.drawCircle(levelData.teleport[i].spawnX, levelData.teleport[i].spawnY, 5);
         staticLevelContainer.addChild(obj4)
+        let label = new PIXI.Text(levelData.teleport[i].destination, {fontFamily: 'Arial', fontSize: 8, fill: 0x000000, align: 'center'});
+            label.x = levelData.teleport[i].bounding[0][0]
+            label.y = levelData.teleport[i].bounding[0][1]
+            staticLevelContainer.addChild(label);
 
     }
+
+    for (let i = 0; i < levelData.fog.length; i ++) {
+        let fogObj = new PIXI.Graphics();
+        fogObj.beginFill(0xa6a6a6);
+        fogObj.name = levelData.fog[i];
+        fogObj.lineStyle(0, 0x00000000);
+        let filter = new PIXI.filters.BlurFilter(30);
+        fogObj.filters = [filter];
+        fogObj.drawRect(levelData.fog[i][0][0], levelData.fog[i][0][1], levelData.fog[i][1][0]-levelData.fog[i][0][0], levelData.fog[i][1][1]-levelData.fog[i][0][1]);
+        engine.stage.addChild(fogObj)
+    }
+    
+
 }
 else if (playerInfo.displayType = "graphic"){
     staticLevelContainer.addChild(backgroundImage);
@@ -216,6 +251,7 @@ function create() {
         if (event.defaultPrevented) {
           return; // Do nothing if the event was already processed
         }
+        if (event.repeat) {return}
         if (!keys.includes(event.key)) {
         keys.push(event.key);
         }
@@ -228,12 +264,17 @@ function create() {
             let screeshot = engine.renderer.view.toDataURL();
             // https://onlinepngtools.com/convert-data-uri-to-png
         navigator.clipboard.writeText(screeshot);
-        engine.stage.scale = new PIXI.Point((3000/window.innerWidth)*1.5, (3000/window.innerHeight)*1.5);
+        engine.stage.scale = new PIXI.Point((2000/window.innerWidth)*1.5, (2000/window.innerHeight)*1.5);
 
         }
 
         if (event.key === "f") {
-            playerInfo.fly = !playerInfo.fly;
+            canFly = !canFly;
+            saveCookie();
+        }
+
+        if (event.key === "g") {
+            disableFog = !disableFog;
             saveCookie();
         }
 
@@ -352,13 +393,37 @@ function create() {
     render();
 } // create
 
-let camHeight:number = (3000/window.innerHeight)*-1.5; // 
-let camWidth:number = (3000/window.innerWidth)*-1.5;
+let camHeight:number = (2000/window.innerHeight)*-1.5; // 
+let camWidth:number = (2000/window.innerWidth)*-1.5;
 function update() {
     fpsMeter.updateTime();
+
+    let fogZone = -1;
+    for (let i = 0; i < levelData.fog.length; i++) {
+        if (player.x > levelData.fog[i][0][0]
+            && player.y > levelData.fog[i][0][1]
+            && player.x + player.width/2 < levelData.fog[i][1][0]
+            && player.y + player.height/2 < levelData.fog[i][1][1]) {
+            fogZone = i;
+            break;
+        }
+    }
+
+    for (let j = 0; j < engine.stage.children.length; j++) {
+        if (engine.stage.children[j].name === levelData.fog[fogZone]) {
+            engine.stage.children[j].visible = false;
+        }else {
+            engine.stage.children[j].visible = true;
+            if (disableFog && playerInfo.dev && !isNaN(parseFloat(engine.stage.children[j].name))) {
+                engine.stage.children[j].visible = false;
+            }
+        }
+    }
+
+
+
     
-    if (playerInfo.dev && playerInfo.fly) {
-        console.log("devmode");
+    if (playerInfo.dev && canFly) {
         if (keys.includes(playerInfo.leftKey)) {
             player.x -= 15;
         }
@@ -458,70 +523,52 @@ if (playerInfo.leftWall) {
         }
     }
 
+
+
+
     for (let i = 0; i < levelData.respawnPoint.length; i++) {
         if (player.x + player.width > levelData.respawnPoint[i][0]
             && player.y + player.height > levelData.respawnPoint[i][1]
             && player.x  < levelData.respawnPoint[i][0]
             && player.y  < levelData.respawnPoint[i][1])
             {
-                console.log("respawn point set", [levelData.respawnPoint[i][0], levelData.respawnPoint[i][1]]);
                 playerInfo.lastSavePoint = [levelData.respawnPoint[i][0]-player.width/2, levelData.respawnPoint[i][1]-player.height/2];
                 break;
             }
     }
-    // |--------------- detect death -------------------|
-    for (let i = 0; i < levelData.deathBox.length; i++) {
-        if (player.x + player.width > levelData.deathBox[i][0][0]
-            && player.y + player.height > levelData.deathBox[i][0][1]
-            && player.x  < levelData.deathBox[i][1][0]
-            && player.y < levelData.deathBox[i][1][1]
-            )
-            {
-                player.x = playerInfo.lastSavePoint[0];
-                player.y = playerInfo.lastSavePoint[1];
-                playerInfo.vx = 0;
-                playerInfo.vy = 0;
-                keys = [];
-                break;
-            }
-    }
 
-
-    let flip:number;
-            if (playerInfo.vy > 0) {
-                flip = 0;
-            }else{flip =1}
-    
-    let playeryplayerheightplayerInfovyflip1 = player.y+player.height+(playerInfo.vy*flip)-1
 
     let leftwallfound = false;
     let rightwallfound = false;
     let groundfound = false;
     let rooffound = false;
     for (var i = 0; i < levelData.phyBox.length; i++) {
-        if (!leftwallfound && player.y >= levelData.phyBox[i][0][1] 
-            && playeryplayerheightplayerInfovyflip1 <= levelData.phyBox[i][1][1] 
-            ) { 
-            if (player.x - 6 < levelData.phyBox[i][1][0] && player.x - 6 > levelData.phyBox[i][0][0]) {
+        if (!leftwallfound
+            && player.x >= levelData.phyBox[i][0][0]
+            && player.y  + player.height*(3/4) > levelData.phyBox[i][0][1]
+            && player.x  <= levelData.phyBox[i][1][0]
+            && player.y +  player.height/4 < levelData.phyBox[i][1][1]
+            ) {
+                console.log("leftwall");
                 playerInfo.leftWall = true;
-
-                player.x = levelData.phyBox[i][1][0]+3;
-                if (playerInfo.vx > 0) {playerInfo.vx = 0;}
+                console.log(leftwallfound)
                 leftwallfound = true;
-            }else{playerInfo.leftWall = false;}}
-            // |---------------------- cheak right wall --------------------|
-        if (!rightwallfound && player.y - 10 >= levelData.phyBox[i][0][1] 
-            && player.y +10 <= levelData.phyBox[i][1][1]) { 
-            if (player.x + player.width + 6 < levelData.phyBox[i][1][0] 
-                && player.x +  player.width + 6 > levelData.phyBox[i][0][0]
+                player.x = levelData.phyBox[i][1][0];
+                if (playerInfo.vx > 0) {playerInfo.vx = 0;}
                 
-                ) {
+            }else{playerInfo.leftWall = false;}
+            // |---------------------- cheak right wall --------------------|
+        if (!rightwallfound && player.x + player.width > levelData.phyBox[i][0][0]
+            && player.y  + player.height*(3/4) > levelData.phyBox[i][0][1]
+            && player.x + player.width < levelData.phyBox[i][1][0]
+            && player.y +  player.height/4 < levelData.phyBox[i][1][1]
+            ) {
                 playerInfo.rightWall = true;
                 player.x = levelData.phyBox[i][0][0]+3 - player.width;
                 if (playerInfo.vx < 0) {playerInfo.vx = 0;}
                 rightwallfound = true;
                 
-            }else{playerInfo.rightWall = false;}}
+            }else{playerInfo.rightWall = false;}
             // |---------------------- cheak ground --------------------|
     if (!groundfound && player.y+player.height >= levelData.phyBox[i][0][1] && player.y+player.height <= levelData.phyBox[i][1][1]) { 
         if (player.x+ (player.width-3) > levelData.phyBox[i][0][0] && player.x +3 < levelData.phyBox[i][1][0]) {
@@ -560,6 +607,22 @@ if (playerInfo.leftWall) {
 
 
 
+    // |--------------- detect death -------------------|
+    for (let i = 0; i < levelData.deathBox.length; i++) {
+        if (player.x + player.width > levelData.deathBox[i][0][0]
+            && player.y + player.height > levelData.deathBox[i][0][1]
+            && player.x  < levelData.deathBox[i][1][0]
+            && player.y < levelData.deathBox[i][1][1]
+            )
+            {
+                player.x = playerInfo.lastSavePoint[0];
+                player.y = playerInfo.lastSavePoint[1];
+                playerInfo.vx = 0;
+                playerInfo.vy = 0;
+                keys = [];
+                break;
+            }
+    }
 
     for (let i = 0; i < 4; i++) {
     if (playerInfo.vx < 0) {
